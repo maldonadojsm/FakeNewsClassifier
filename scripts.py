4ba07b4189c8f3ffe65e6260dcd0f607a260c62b
Juan Maldonado <maldonadojsm@gmail.com>
2020-07-22 01:11:41 -0400

    Push Model & Pre Processing File

---
+++ scripts.py
@@ -0,0 +1,21 @@
+# !/usr/bin/env python
+# title           :scripts.py
+# description     :Enter Description Here
+# author          :Sebastian Maldonado
+# date            :7/15/2020
+# version         :0.0
+# usage           :SEE README.md
+# notes           :Enter Notes Here
+# python_version  :3.6.8
+# conda_version   :4.8.3
+# =================================================================================================================
+
+
+def check_size(d1, d2):
+    """
+    Check if dataset size is the same after dataset cleaning
+    :param d1: Original Dataset Size
+    :param d2: Cleaned Dataset Size
+    :return: Return boolean. True
+    """
+    return True if d1 == d2 else False
